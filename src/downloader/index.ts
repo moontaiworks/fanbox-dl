c975d0b9edcac4876275479447c5a601e0e55780
moontai0724 <moontai0724@gmail.com>
2026-06-03 07:42:19 +0000

    refactor(cli): move cli into another tsconfig project with node type

--- src/downloader/index.ts
+++ src/downloader/index.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import { runCli } from "./downloader/cli.js";
+import { runCli } from "./cli.js";
 
 void runCli(process.argv.slice(2)).then((exitCode) => {
   process.exitCode = exitCode;
