a44ea3e0449e034daa0daf9edd292c6e9799ec4b
moontai0724 <moontai0724@gmail.com>
2026-06-04 07:46:42 +0000

    ci: remove path aliases

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -26,7 +26,6 @@ export default [
             ["parent", "sibling", "index"],
             ["object", "unknown"],
           ],
-          internalPattern: [/^(~\/|#)/.source],
         },
       ],
     },
